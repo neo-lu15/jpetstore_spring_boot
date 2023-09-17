1bb193b63d94704fb97649151a4e771ea6d63702
neo-lu15 <neo901115@gmail.com>
2023-09-17 23:04:14 +0800

    keycloak link to app

--- app/src/main/java/com/kazuki43zoo/jpetstore/config/WebSecurityConfig.java
+++ app/src/main/java/com/kazuki43zoo/jpetstore/config/WebSecurityConfig.java
@@ -1,45 +1,45 @@
-/*
- *    Copyright 2016-2023 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package com.kazuki43zoo.jpetstore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * @author Kazuki Shimizu
- */
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin()
-				.loginPage("/login")
-				.defaultSuccessUrl("/catalog")
-				.permitAll();
-		http.logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/catalog")
-				.deleteCookies("JSESSIONID")
-				.permitAll();
-		http.authorizeRequests()
-				.mvcMatchers("/my/**").authenticated()
-				.anyRequest().permitAll();
-	}
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+            .oauth2Client()
+            .and()
+            .oauth2Login()
+            .tokenEndpoint()
+            .and()
+            .userInfoEndpoint()
+            .and() // Add this line to close the userInfoEndpoint() configuration
+            .permitAll() // Allow access to userInfo endpoint without authentication
+            .defaultSuccessUrl("/catalog");
 
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+
+        http
+            .authorizeRequests()
+            .antMatchers("/my/**").permitAll() // Permit access to /my/** without authentication
+            .anyRequest()
+            .fullyAuthenticated()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/catalog")
+            .deleteCookies("JSESSIONID")
+            .permitAll();
+
+        return http.build();
+    }
 }
+
