826b37694657095f2416142c89dc91d3968b80f6
neo-lu15 <71438352+neo-lu15@users.noreply.github.com>
2023-10-28 19:20:53 +0800

    Update WebSecurityConfig.java

--- app/src/main/java/com/kazuki43zoo/jpetstore/config/WebSecurityConfig.java
+++ app/src/main/java/com/kazuki43zoo/jpetstore/config/WebSecurityConfig.java
@@ -1,45 +1,72 @@
+/*
+ *    Copyright 2016-2023 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.kazuki43zoo.jpetstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-            .oauth2Client()
-            .and()
-            .oauth2Login()
-            .tokenEndpoint()
-            .and()
-            .userInfoEndpoint()
-            .and() // Add this line to close the userInfoEndpoint() configuration
-            .permitAll() // Allow access to userInfo endpoint without authentication
-            .defaultSuccessUrl("/catalog");
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
 
-        http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-
-        http
-            .authorizeRequests()
-            .antMatchers("/my/**").permitAll() // Permit access to /my/** without authentication
+    @Order(1)
+    @Bean
+    public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/")
+            .permitAll()
             .anyRequest()
-            .fullyAuthenticated()
+            .authenticated();
+        http.oauth2Login()
             .and()
             .logout()
-            .logoutSuccessUrl("/catalog")
-            .deleteCookies("JSESSIONID")
-            .permitAll();
-
+            .logoutSuccessUrl("/");
         return http.build();
     }
-}
 
+    @Order(2)
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/*")
+            .hasRole("USER")
+            .anyRequest()
+            .authenticated();
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+            .build();
+    }
+}
